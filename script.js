c0a253c6cd3e23e3870bbfbe17653a9cc7eae4a0
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-16 18:00:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventListener('click', displayMessage)
+/* document.getelementbyid connects the function to the id in HTML, while the event listener will display the message when the button is clicked*/ 
+
+function displayMessage () {
+  alert("Hello")
+}
